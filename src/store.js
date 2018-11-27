f50d5b2aa2e0f3c8b05693891a2fb5efa23e0e38
Glitch (book-trader-bb-jb) <none>
2018-11-27 22:58:56 +0000

    🎲🚩 Glitch: Figured out add & delete myBooks

--- src/store.js
+++ src/store.js
@@ -14,12 +14,13 @@ let store = new Vuex.Store({
     loggedInUser: null,
     count: 1,
     book: null,
-    books: []
+    books: [],
+    myBooks: [],
+    allBooks: []
   },
   getters: {
     getLoggedInUser: state => {
-      const localStore = JSON.parse(localStorage.getItem('book-trader-bb-jb-token'));    
-      console.log(localStore);
+      const localStore = JSON.parse(localStorage.getItem('book-trader-bb-jb-token'));          
       if (localStore) {
         state.jsonWebToken = localStore.token
         state.loggedInUser = localStore.loggedInUser
@@ -33,10 +34,12 @@ let store = new Vuex.Store({
     getBooks: state => {
       state.books
     },
+    getMyBooks: state => {
+      state.myBooks
+    },
     isAuth () {
       const localStore = JSON.parse(localStorage.getItem('book-trader-bb-jb-token'));
       if (localStore) {
-        console.log("CURR USR ID ", localStore.loggedInUser)
         return JSON.parse(localStorage.getItem('book-trader-bb-jb-token')).token !== null;
       } else {
         return false;
@@ -72,6 +75,21 @@ let store = new Vuex.Store({
     },
     setBooks (state, books) {
       state.books = books;
+    },
+    saveBookInformation (state, book) { // add a new book to my books (save to Firebase)
+      let newState = state.myBooks;
+      newState.push(book);
+      console.log(state.myBooks);
+      state.myBooks = newState;      
+    },
+    setMyBooks (state, books) { // get all my books (coming from Firebase)
+      state.myBooks = books
+    },
+    clearSearchResults (state) {
+      state.books = []
+    },
+    clearMyBooks (state) {
+      return state.myBooks = []
     }
   },
   actions: {
@@ -80,7 +98,7 @@ let store = new Vuex.Store({
         .then((res) => {
 
           var userRef = firebase.database().ref("users");
-          console.log(userRef);
+          // console.log(userRef);
           userRef.push({ uid : res.user.uid,
              username: payload.username,
              email: payload.email,
@@ -101,23 +119,21 @@ let store = new Vuex.Store({
        
     },
     signIn ({commit}, payload) {
-      console.log('Sign in attempt', payload.email)
+      // console.log('Sign in attempt', payload.email)
       firebase.auth().signInWithEmailAndPassword(payload.email, payload.password)
       .then(res => {
 
         let currentUser = '';
         var usersRef = firebase.database().ref('users');
         var test = usersRef.orderByChild("email").equalTo(res.user.email);
-              // console.log('find',test);
-
+             
           usersRef.on("value", function(snapshot) {
             snapshot.forEach(function(childSnapshot) {
 
               var key = childSnapshot.key;
               var childData = childSnapshot.val();
-             // console.log(childData.uid, res.user.uid)
-              if(childData.uid === res.user.uid) {
-                //console.log(childData.username);
+             
+              if(childData.uid === res.user.uid) {             
                 currentUser = childData.username;
               }
             })
@@ -156,7 +172,7 @@ let store = new Vuex.Store({
         parser.parseString(response.data,
           function(err, result) {
               if (err) console.log(err);
-              console.log(result.GoodreadsResponse.book);
+              //console.log(result.GoodreadsResponse.book);
               // cb(null, result.GoodreadsResponse.book);
               // call the mutation to set the book object (in state)
               commit('setBook', result.GoodreadsResponse.book)
@@ -170,28 +186,94 @@ let store = new Vuex.Store({
       const apiKey = process.env.VUE_APP_BOOK_READS_API;
       const api = `https://cors-anywhere.herokuapp.com/https://www.goodreads.com/search/index.xml?format=xml&key=${apiKey}&q=${searchTerm}`
       axios.get(api).then((response) => {
-        //console.log(response.data)
+        
         parser.parseString(response.data,
           function(err, result) {
               if (err) console.log(err);
-              console.log(result.GoodreadsResponse.search.results.work);  
               if (result.GoodreadsResponse.search.results.work)
-                commit('setBooks', result.GoodreadsResponse.search.results.work);
-              // call the mutation to set the book object (in state)
-              // commit('setBook', result.GoodreadsResponse.book)
+                commit('setBooks', result.GoodreadsResponse.search.results.work);        
           });
       })
     },
     saveBook({commit}, book) {
-      // save a book to my books collection
+      const user_id = this.state.userId;
+
+      let fbBookToAdd = {}, nodeName = {}; 
+      nodeName[user_id] = {
+            bookTitle: book.best_book.title,
+            authorName: book.best_book.author.name,
+            bookId: book.best_book.id._,
+            photoURL: book.best_book.image_url,
+          }
+      
+      fbBookToAdd = nodeName;
+      console.log('adding book in store:' + fbBookToAdd);
+      
+      if (!user_id) return;
+      var bookRef = firebase.database().ref('books');
+      bookRef.push(fbBookToAdd).then((snap) => {
+        console.log('saving book - promise')
+        commit('saveBookInformation', fbBookToAdd);      
+      });
+      
     },
-    getmyBooks({commit}) {
+    deleteBook({commit, state}, bookId) {
+      var booksRef = firebase.database().ref('books');
+      //console.log(booksRef.child(['.key']));
+      
+//       console.log('Deleting:', bookId)
+//       // Create a reference to the books collection
+
+      
+      booksRef.once('value')
+      .then(function(snapshot) {
+        var fbId = "";
+        snapshot.forEach(function(childSnapshot) {
+            // childSnapshot.remove();
+          console.log(childSnapshot.key);
+          var oneBook = childSnapshot.val();
+          if (oneBook.hasOwnProperty(state.userId)) {
+            // the user's book to delete
+            if (oneBook[state.userId].bookId == bookId) { 
+              fbId = childSnapshot.key;
+              booksRef.child(fbId).remove().then(() => { 
+                console.log(fbId, ' deleted')
+                alert('Your book was deleted')
+                // todo: update state
+              })                                            
+            }
+          }
+        })
+      })
+    },
+    getMyBooks({commit}) {
       // get all my books available to trade
+      var bookRef = firebase.database().ref('books');
+      let tempArray = [];
+      bookRef.once('value')
+      .then(function(snapshot) {
+        snapshot.forEach(function(childSnapshot) {
+          
+          tempArray.push(childSnapshot.val())
+          //console.log(tempArray)
+        })
+        commit('setMyBooks', tempArray)         
+      })
+      
     },
     getOtherUserBooks({commit}, userId) {
       // show all available books for trade minus my books
     },
     showTrades() {
+      
+    },
+    clearSearch({commit}) {
+      // clear search results from state
+      commit('clearSearchResults')       
+    },
+    clearMyBooks({commit}) {
+      // clear myBooks from state
+      return commit('clearMyBooks')       
     }
   }
 })
